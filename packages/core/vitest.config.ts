7df66c64d9426c53f38690ff98eacf3a6c1aef5b
magurouhiru <73399968+magurouhiru@users.noreply.github.com>
2026-07-04 11:43:40 +0000

    test

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node", // これを明示
-    pool: "forks",
+    pool: "threads",
     testTimeout: 10000,
     isolate: false,
     maxWorkers: 1,
