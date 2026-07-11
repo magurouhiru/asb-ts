86731cad63f2865302d91a461f0f25d88dbfae92
magurouhiru <73399968+magurouhiru@users.noreply.github.com>
2026-07-11 02:47:53 +0000

    fix

--- test-bot-image.sh
+++ test-bot-image.sh
@@ -3,4 +3,5 @@
 # 1. エラー発生時に処理を中断する設定
 set -euo pipefail
 
-docker run --rm --env-file ./apps/discord-bot/.env asb_ts__discord_bot:latest
+make
+docker run --rm --env-file ./apps/discord-bot/.env asb-ts_discord-bot:latest
