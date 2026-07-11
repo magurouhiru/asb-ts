0c09c0f056c2ed1dc55fa140dca2b97a4e027959
magurouhiru <73399968+magurouhiru@users.noreply.github.com>
2026-07-11 03:44:44 +0000

    fix

--- Makefile
+++
@@ -1,4 +0,0 @@
-create_image: create_image_discord_bot
-
-create_image_discord_bot:
-	docker build -t asb-ts_discord-bot:latest -f discord-bot.Dockerfile .
--- test-bot-image.sh
+++ test-bot-image.sh
@@ -3,5 +3,5 @@
 # 1. エラー発生時に処理を中断する設定
 set -euo pipefail
 
-make
+docker build -t asb-ts_discord-bot:latest -f discord-bot.Dockerfile .
 docker run --rm --env-file ./apps/discord-bot/.env asb-ts_discord-bot:latest
